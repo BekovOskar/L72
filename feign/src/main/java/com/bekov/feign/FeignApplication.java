b1fdfb0ce4c4228ac88b6635a926d13dadd4d6b6
BekovOskar <bek.offsll@gmail.com>
2019-06-18 16:08:22 +0600

    added added settings zipkin

--- feign/src/main/java/com/bekov/feign/FeignApplication.java
+++ feign/src/main/java/com/bekov/feign/FeignApplication.java
@@ -1,9 +1,11 @@
 package com.bekov.feign;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -14,4 +16,8 @@ public class FeignApplication {
         SpringApplication.run(FeignApplication.class, args);
     }
 
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
+    }
 }
